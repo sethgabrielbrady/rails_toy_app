04331db1ec97c3412b8e4b9e09f148b587077a83
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-06-26 08:37:09 -0400

    scoffold Microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   def hello
-    render html: "hello, world!"
+    render html: "hello, rails!"
   end
 end
